ad18caa0a0e7e2a9f940dd6a492efcf0cca8287b
sanskruti-DL <sanskruti.gosake@definelabs.com>
2026-05-03 04:07:15 +0530

    DB updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,13 @@
 const {Pool} = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString : process.env.DATABASE_URL,
+  ssl: {rejectUnauthorized : false}
 });
 
 pool.connect()
 .then(() => console.log("Db connected"))
-.catch(err => console.error("not connected",err.message));
+.catch(err => console.error("not connected POSTGRES",err.message));
 
 module.exports  = pool;
 
